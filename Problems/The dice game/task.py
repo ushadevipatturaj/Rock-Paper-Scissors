3d6bfd7f50df8f11d6f63a9b57e9422ccb16de37
ushapatturaj <ushadevipatturaj@gmail.com>
2020-07-05 19:31:03 +0200

    Dice Game

---
+++ Problems/The dice game/task.py
@@ -0,0 +1,8 @@
+import random
+
+# this line is needed for us to check the results, don't modify it please
+random.seed(int(input()))
+choice = random.randint(1, 6)
+
+# use a function from the random module in the next line
+print(choice)
